d47698420c96e74cdfc9ede3a1bd01991b9276fd
ubuntu <legol.chen@gmail.com>
2017-05-07 01:49:07 -0700

    main

---
+++ test/test_folly.cpp
@@ -0,0 +1,25 @@
+#include <folly/futures/Future.h>
+#include <iostream>
+
+using namespace folly;
+using namespace std;
+
+void foo(int x) {
+  // do something with x
+  cout << "foo(" << x << ")" << endl;
+}
+
+main() {
+
+  cout << "making Promise" << endl;
+  Promise<int> p;
+  Future<int> f = p.getFuture();
+  f.then(foo);
+  cout << "Future chain made" << endl;
+
+// ... now perhaps in another event callback
+
+  cout << "fulfilling Promise" << endl;
+  p.setValue(42);
+  cout << "Promise fulfilled" << endl;
+}
